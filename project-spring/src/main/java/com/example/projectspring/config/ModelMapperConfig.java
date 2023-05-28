af749b6287147b363cf67f0a19bb3b20360cccb0
Stoyan <gabrovskiStoyan@gmail.com>
2023-05-28 22:37:28 +0300

    Modifying and code formatting

---
+++ project-spring/src/main/java/com/example/projectspring/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.example.projectspring.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class  {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper mm = new ModelMapper();
+        mm.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return mm;
+    }
+}
---
+++ project-spring/src/main/java/com/example/projectspring/dto/CarDto.java
@@ -0,0 +1,2 @@
+package com.example.projectspring.dto;public class CarDto {
+}
